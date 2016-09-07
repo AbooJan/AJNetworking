68fdf0bc6fcc765df808b307c18c081bb3316153
AbooJan <aboojaner@gmail.com>
2016-09-08 00:10:55 +0800

    1

--- AJNetworking/Demo/ViewController.m
+++ AJNetworking/Demo/ViewController.m
@@ -45,31 +45,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
 }
 
 #pragma mark GET请求示例
 - (IBAction)loginBtnClick:(UIButton *)sender
 {
     [self.view endEditing:YES];
-    
+
     RequestBeanDemoLogin *requestBean = [[RequestBeanDemoLogin alloc] init];
     requestBean.account = self.accountTF.text;
     requestBean.pw = self.pwTF.text;
-    
+
     //test
     requestBean.name = @"aboo";
-    
+
     [AJNetworkManager requestWithBean:requestBean callBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-        
+
         if (!err) {
-            
+
             // 返回结果处理
             ResponseBeanDemoLogin *response = responseBean;
             AJLog(@"user: %@", response.data);
-            
+
         }else{
-            
+
             if (responseBean) {
                 AJLog(@"请求错误：%ld -- %@", responseBean.statusCode, responseBean.responseMessage);
             }else{
@@ -83,60 +83,60 @@
 - (IBAction)registerBtnClick:(id)sender
 {
     [self.view endEditing:YES];
-    
+
     RequestBeanDemoRegister *requestBean = [RequestBeanDemoRegister new];
     requestBean.userName = self.userNameTF.text;
     requestBean.pw = self.pwTF1.text;
-    
+
     [AJNetworkManager requestWithBean:requestBean callBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-        
+
         if (!err) {
-            
+
             // 结果处理
             ResponseBeanDemoRegister *response = responseBean;
             AJLog(@"userId:%@", response.data.userId);
         }
     }];
-    
+
 }
 
 #pragma mark 短期缓存示例
 - (IBAction)readNewsBtnClick:(id)sender
 {
     [self.view endEditing:YES];
-    
+
     RequestBeanDemoNews *requestBean = [RequestBeanDemoNews new];
     requestBean.userId = self.userIdTF.text;
     requestBean.dateTime = self.dateTimeTF.text;
-    
+
     [AJNetworkManager requestWithBean:requestBean cacheCallBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-        
+
         if (!err) {
-            
+
             // 读取缓存
             AJLog(@"###来自缓存###");
             [self handleNews:responseBean];
-            
+
         }else{
             // 读取缓存失败
             AJLog(@"%@", [err description]);
         }
-        
+
     } httpCallBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-        
+
         if (!err) {
-            
+
             // 网络请求成功
             AJLog(@"###来自网络###");
             [self handleNews:responseBean];
-            
+
         }else{
             // 网络请求失败
             AJLog(@"%@", [err description]);
         }
-        
+
     }];
-    
+
 }
 
 - (void)handleNews:(ResponseBeanDemoNews *)news
@@ -150,34 +150,30 @@
 - (IBAction)filmBtnClick:(id)sender
 {
     NSInteger testCode = self.statusSegment.selectedSegmentIndex;
-    
+
     RequestBeanDemoFilm *reqeustBean = [[RequestBeanDemoFilm alloc] init];
     reqeustBean.testCode = testCode;
-    
-<<<<<<< .merge_file_x1btWp
-    [AJNetworkManager requestWithBean:reqeustBean cacheCallBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-=======
+
     /*
      * 对于服务器增量更新的设计，可以先读取缓存显示出来，然后根据http请求结果来判断是否更新页面。
      */
-    
-    [AJNetworkManager requestWithBean:reqeustBean cacheCallBack:^(__kindof ResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
->>>>>>> .merge_file_1QTkPv
-        
+     
+    [AJNetworkManager requestWithBean:reqeustBean cacheCallBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
+
         if (!err) {
-            
+
             AJLog(@"---来自缓存---");
             [self handleFilm:responseBean];
         }
-        
+
     } httpCallBack:^(__kindof AJResponseBeanBase * _Nullable responseBean, AJError * _Nullable err) {
-        
+
         if (!err) {
-            
+
             AJLog(@"---来自网络---");
             [self handleFilm:responseBean];
         }
-        
+
     }];
 }
 
